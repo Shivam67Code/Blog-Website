8b57d362f92d83b9aec8cffb92bda436a2eb5231
Shivam67Code <evilsniper64@gmail.com>
2025-09-28 16:08:44 +0545

    Remove unnecessary blank line in user routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -37,7 +37,6 @@ router.route("/login").post(loginUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/forgot-password").post(forgotPassword)
 router.route("/reset-password/:token").post(resetPassword)
-
 // PROTECTED ROUTES (Authentication required in these routes)
 router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/change-password").post(verifyJWT, changePassword)
