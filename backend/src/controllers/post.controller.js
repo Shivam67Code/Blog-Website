ad8b03c742a4f5b74b6b4ee6c35b62eec9d7a5da
Shivam67Code <evilsniper64@gmail.com>
2025-09-28 12:37:36 +0545

    Completeed contorllers of POST

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { Post } from "../models/post.model.js"
-import { uploadOnCloudinary } from "../utils/cloudinary"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 // create post
 const createPost = asyncHandler(async (req, res) => {
@@ -121,6 +121,112 @@ const updatePost = asyncHandler(async (req, res) => {
 })
 
 // delete post
+const deletePost = asyncHandler(async (req, res) => {
+  const { postId } = req.params;
+  const post = await Post.findById(postId)
+
+  if (!post) {
+    throw new ApiError(404, "Post not found")
+  }
+
+  if (post.author.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You aer not authorized to Delete this Post")
+  }
+
+  await Post.findByIdAndDelete(postId)
+
+  return res.status(200)
+    .json(
+      new ApiResponse(200, {}, "Post Deleted SuccessFullly")
+    )
+})
 // getpostByAuthor
+const getPostsByAuthor = asyncHandler(async (req, res) => {
+  const { authorId } = req.params;
+  const { page = 1, limit = 10 } = req.query
+
+  const posts = await Post.find({ author: authorId })
+    .populate('author', 'username email avatar')
+    .sort({ createdAt: -1 }) // show neweset first
+    .limit(limit * 1)
+    .skip((page - 1) * limit)
+    .exec()
+
+  const totalPosts = await Post.countDocuments({ author: authorId })
+
+  return res.status(200).json(
+    new ApiResponse(200, {
+      posts,
+      totalPosts,
+      totalPages: Math.ceil(totalPosts / limit),
+      currentPage: page
+    }, "Author Posts Fetched SuccessFully")
+  )
+})
 // getMYPosts
+const getMyPosts = asyncHandler(async (req, res) => {
+  const { page = 1, limit = 10 } = req.query
+
+  const posts = await Post.find({ author: req.user._id })
+    .populate('author', 'username email avatar')
+    .sort({ createdAt: -1 })
+    .limit(limit * 1)
+    .skip((page - 1) * limit)
+    .exec()
+
+  const totalPosts = await Post.countDocuments({ author: req.user._id })
+
+  return res
+    .status(200)
+    .json(new ApiResponse(
+      200,
+      {
+        posts,
+        totalPosts,
+        totalPages: Math.ceil(totalPosts / limit),
+        currentPage: page,
+      }, "Your Posts fetched SuccessFully"
+    ))
+
+})
 // togglePostLiek
+const togglePostLike = asyncHandler(async (req, res) => {
+  const { postId } = req.params
+  const userId = req.user._id
+
+  const post = await Post.findById(postId)
+  if (!post) {
+    throw new ApiError(404, "Post not found");
+  }
+  const isLiked = post.likes.includes(userId);
+
+  if (isLiked) {
+    //if already liked then remove the like
+    post.likes = post.likes.filter(id => id.toString() !== userId.toString())
+  } else {
+    // if not liked then add the like
+    post.likes.push(userId)
+  }
+  await post.save()
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, {
+        isLiked: !isLiked,
+        likesCount: post.likes.length
+      }, isLiked ? " Post unliked SuccessFully" : "Post liked Successfully")
+    )
+
+})
+
+export {
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  getPostsByAuthor,
+  getMyPosts,
+  togglePostLike
+}
